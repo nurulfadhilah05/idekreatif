20cbc3e36e5b391d5546b509675767e1846f687d
nurulfadhilah05 <nurul.fadhilah1078@smk.belajar.id>
2025-01-24 13:26:58 +0700

    menambah konfigurasi koneksi

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Konfigurasi koneksi database
+$host = "localhost"; // Nama host server database
+$username = "root"; // Username untuk akses ke database
+$password = "idekreatif"; // Nama database yang digunakan
+
+// Membuat koneksi ke databse menggunakan MySQLi
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Mengecek apakah koneksi berhasil 
+if ($conn->connect_eror) {
+    // Menampilkan pesan eror jika gagal
+    die("Database gagal terkoneksi: " . $conn->connect_eror);
+}
+
+//Jika koneksi berhasil, script akan terus berjalan tanpa pesan eror
+?>
